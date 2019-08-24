e9f5814a8a1b8fb6d86ec24534f041caf3aca70b
jmazionis <julius.mazionis@gmail.com>
2019-08-24 04:14:30 +0300

    update integration tests

--- internal/api/integration-tests/car_plate_integration_test.go
+++ internal/api/integration-tests/car_plate_integration_test.go
@@ -24,107 +24,20 @@ func init() {
 	beego.TestBeegoInit(apppath)
 }
 
-func TestAddCarplate(t *testing.T) {
-
-	Convey("Subject: Test AddCarplate Endpoint\n", t, func() {
-
-		Convey("Given valid request", func() {
-			rBody, err := json.Marshal(map[string]interface{}{
-				"plateId":   "AAA-200",
-				"modelName": "Subaru Outback",
-				"modelYear": 1990,
-				"owner":     "Driver 2",
-			})
-
-			if err != nil {
-				beego.Error(err)
-			}
-
-			r, _ := http.NewRequest("POST", "/api/carplates", bytes.NewBuffer(rBody))
-			w := httptest.NewRecorder()
-			beego.BeeApp.Handlers.ServeHTTP(w, r)
-
-			beego.Trace("test", "TestAddCarplate", "Code[%d]\n%s", w.Code, w.Body.String())
-
-			Convey("Status code should Be 201", func() {
-				So(w.Code, ShouldEqual, 201)
-			})
-
-			Convey("Should include location header", func() {
-				So(len(w.Header().Get("location")), ShouldBeGreaterThan, 0)
-			})
-
-			Convey("The body should be empty", func() {
-				So(w.Body.Len(), ShouldBeZeroValue)
-			})
-		})
-
-		Convey("Given invalid request", func() {
-			rBody, err := json.Marshal(map[string]interface{}{
-				"plateId":   "AAA-20",
-				"modelName": "Subaru Impreza",
-				"modelYear": 1990,
-			})
-
-			if err != nil {
-				beego.Error(err)
-			}
-
-			r, _ := http.NewRequest("POST", "/api/carplates", bytes.NewBuffer(rBody))
-			w := httptest.NewRecorder()
-			beego.BeeApp.Handlers.ServeHTTP(w, r)
-
-			beego.Trace("test", "TestAddCarplate", "Code[%d]\n%s", w.Code, w.Body.String())
-
-			Convey("Status code should Be 400", func() {
-				So(w.Code, ShouldEqual, 400)
-			})
-		})
-
-		Reset(resetStorage)
-
-	})
-}
-
-func TestGetCarplates(t *testing.T) {
-	r, _ := http.NewRequest("GET", "/api/carplates", nil)
-	w := httptest.NewRecorder()
-	beego.BeeApp.Handlers.ServeHTTP(w, r)
-
-	beego.Trace("test", "TestGetCarplates", "Code[%d]\n%s", w.Code, w.Body.String())
-
-	Convey("Subject: Test GetCarplates Endpoint\n", t, func() {
-
-		Convey("Status Code Should Be 200", func() {
-			So(w.Code, ShouldEqual, 200)
-		})
-
-		Convey("The Result Should Not Be Empty", func() {
-			So(w.Body.Len(), ShouldBeGreaterThan, 0)
-		})
-
-	})
-}
-
 func TestGetCarplate(t *testing.T) {
-
-	Convey("Subject: Test GetCarplate Endpoint\n", t, func() {
-
+	Convey("Subject: Test GetCarplate endpoint", t, func() {
 		Convey("Given non existing carplate", func() {
 			r, _ := http.NewRequest("GET", "/api/carplates/1", nil)
 			w := httptest.NewRecorder()
-			beego.BeeApp.Handlers.ServeHTTP(w, r)
 
-			beego.Trace("test", "TestGetCarplate", "Code[%d]\n%s", w.Code, w.Body.String())
+			beego.BeeApp.Handlers.ServeHTTP(w, r)
 
 			Convey("Status code should be 404", func() {
 				So(w.Code, ShouldEqual, 404)
 			})
-
 			Convey("The body should be empty", func() {
 				So(w.Body.Len(), ShouldBeZeroValue)
 			})
-
 		})
 
 		Convey("Given existing carplate", func() {
@@ -141,22 +54,220 @@ func TestGetCarplate(t *testing.T) {
 
 			beego.BeeApp.Handlers.ServeHTTP(w, r)
 
-			beego.Trace("test", "TestGetCarplate", "Code[%d]\n%s", w.Code, w.Body.String())
-
 			Convey("Status code should be 200", func() {
 				So(w.Code, ShouldEqual, 200)
 			})
-
 			Convey("The body should contain retrieved item", func() {
 				carplate := models.CarPlate{}
 				_ = json.Unmarshal(w.Body.Bytes(), &carplate)
 				So(carplate.ID, ShouldEqual, carplateId)
 			})
+		})
+		Reset(resetStorage)
+	})
+}
 
+func TestGetAllCarplates(t *testing.T) {
+	storages.CarPlateDb().Add(&models.CarPlate{
+		ID: "A",
+	})
+	storages.CarPlateDb().Add(&models.CarPlate{
+		ID: "B",
+	})
+	r, _ := http.NewRequest("GET", "/api/carplates", nil)
+	w := httptest.NewRecorder()
+
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	Convey("Subject: Test GetCarplates endpoint", t, func() {
+		Convey("Given valid request", func() {
+			Convey("Status code should be 200", func() {
+				So(w.Code, ShouldEqual, 200)
+			})
+			Convey("The body should contain retrieved items", func() {
+				carplates := []*models.CarPlate{}
+				_ = json.Unmarshal(w.Body.Bytes(), &carplates)
+				So(len(carplates), ShouldEqual, 2)
+			})
+			Reset(resetStorage)
 		})
 
-		Reset(resetStorage)
+	})
+}
 
+func TestAddCarplate(t *testing.T) {
+	Convey("Subject: Test AddCarplate endpoint", t, func() {
+		Convey("Given valid request", func() {
+			rBody, _ := json.Marshal(map[string]interface{}{
+				"plateId":   "AAA-200",
+				"modelName": "Subaru Outback",
+				"modelYear": 1990,
+				"owner":     "Driver 2",
+			})
+			r, _ := http.NewRequest("POST", "/api/carplates", bytes.NewBuffer(rBody))
+			w := httptest.NewRecorder()
+
+			beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+			Convey("Status code should Be 201", func() {
+				So(w.Code, ShouldEqual, 201)
+			})
+			Convey("Should include location header", func() {
+				So(len(w.Header().Get("location")), ShouldBeGreaterThan, 0)
+			})
+			Convey("The body should be empty", func() {
+				So(w.Body.Len(), ShouldBeZeroValue)
+			})
+		})
+
+		Convey("Given invalid request", func() {
+			rBody, _ := json.Marshal(map[string]interface{}{
+				"plateId":   "AAA-20",
+				"modelName": "Subaru Impreza",
+				"modelYear": 1990,
+			})
+			r, _ := http.NewRequest("POST", "/api/carplates", bytes.NewBuffer(rBody))
+			w := httptest.NewRecorder()
+
+			beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+			errors := struct {
+				Errors []string `json:"errors"`
+			}{}
+			err := json.Unmarshal(w.Body.Bytes(), &errors)
+			if err != nil {
+				beego.Error(err)
+			}
+			Convey("Status code should Be 400", func() {
+				So(w.Code, ShouldEqual, 400)
+			})
+			Convey("The body should contain validation errors summary", func() {
+				So(len(errors.Errors), ShouldEqual, 2)
+			})
+		})
+		Reset(resetStorage)
+	})
+}
+
+func TestUpdateCarplate(t *testing.T) {
+	Convey("Subject: Test UpdateCarplate endpoint", t, func() {
+		Convey("Given valid request", func() {
+			storages.CarPlateDb().Add(&models.CarPlate{
+				ID: "A",
+			})
+			rBody, _ := json.Marshal(map[string]interface{}{
+				"id":        "A",
+				"plateId":   "AAA-200",
+				"modelName": "Subaru Outback",
+				"modelYear": 1990,
+				"owner":     "Driver 2",
+			})
+			r, _ := http.NewRequest("PUT", "/api/carplates", bytes.NewBuffer(rBody))
+			w := httptest.NewRecorder()
+
+			beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+			Convey("Status code should Be 204", func() {
+				So(w.Code, ShouldEqual, 204)
+			})
+			Convey("The body should be empty", func() {
+				So(w.Body.Len(), ShouldBeZeroValue)
+			})
+		})
+
+		Convey("Given non existing resource", func() {
+			storages.CarPlateDb().Add(&models.CarPlate{
+				ID: "A",
+			})
+			rBody, _ := json.Marshal(map[string]interface{}{
+				"id":        "B",
+				"plateId":   "AAA-200",
+				"modelName": "Subaru Outback",
+				"modelYear": 1990,
+				"owner":     "Driver 2",
+			})
+			r, _ := http.NewRequest("PUT", "/api/carplates", bytes.NewBuffer(rBody))
+			w := httptest.NewRecorder()
+
+			beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+			Convey("Status code should Be 404", func() {
+				So(w.Code, ShouldEqual, 404)
+			})
+			Convey("The body should be empty", func() {
+				So(w.Body.Len(), ShouldBeZeroValue)
+			})
+		})
+
+		Convey("Given invalid request", func() {
+			storages.CarPlateDb().Add(&models.CarPlate{
+				ID: "A",
+			})
+			rBody, _ := json.Marshal(map[string]interface{}{
+				"plateId":   "AA-200",
+				"modelName": "Subaru Outback",
+				"modelYear": 1700,
+				"owner":     "Driver 2",
+			})
+			r, _ := http.NewRequest("PUT", "/api/carplates", bytes.NewBuffer(rBody))
+			w := httptest.NewRecorder()
+
+			beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+			errors := struct {
+				Errors []string `json:"errors"`
+			}{}
+			err := json.Unmarshal(w.Body.Bytes(), &errors)
+			if err != nil {
+				beego.Error(err)
+			}
+
+			Convey("Status code should Be 400", func() {
+				So(w.Code, ShouldEqual, 400)
+			})
+			Convey("The body should contain validation errors summary", func() {
+				So(len(errors.Errors), ShouldEqual, 3)
+			})
+		})
+		Reset(resetStorage)
+	})
+}
+
+func TestDeleteCarplate(t *testing.T) {
+	Convey("Subject: Test DeleteCarplate endpoint", t, func() {
+		Convey("Given existing resource", func() {
+			storages.CarPlateDb().Add(&models.CarPlate{
+				ID: "A",
+			})
+			r, _ := http.NewRequest("DELETE", "/api/carplates/A", nil)
+			w := httptest.NewRecorder()
+
+			beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+			Convey("Status code should Be 204", func() {
+				So(w.Code, ShouldEqual, 204)
+			})
+			Convey("The body should be empty", func() {
+				So(w.Body.Len(), ShouldBeZeroValue)
+			})
+		})
+		Convey("Given non existing resource", func() {
+			storages.CarPlateDb().Add(&models.CarPlate{
+				ID: "A",
+			})
+			r, _ := http.NewRequest("DELETE", "/api/carplates/B", nil)
+			w := httptest.NewRecorder()
+
+			beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+			Convey("Status code should Be 204", func() {
+				So(w.Code, ShouldEqual, 204)
+			})
+			Convey("The body should be empty", func() {
+				So(w.Body.Len(), ShouldBeZeroValue)
+			})
+		})
+		Reset(resetStorage)
 	})
 }
 
